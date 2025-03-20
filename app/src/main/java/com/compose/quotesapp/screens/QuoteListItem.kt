2c1d0b8531f2897a484a2552e741cd2f34c968f2
shivam <mandaliashivam72@gmail.com>
2025-03-20 22:59:09 +0530

    setup project by shivam

---
+++ app/src/main/java/com/compose/quotesapp/screens/QuoteListItem.kt
@@ -0,0 +1,50 @@
+package com.compose.quotesapp.screens
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun QuoteListItem() {
+    Card(
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                imageVector = Icons.Filled.Close,
+                colorFilter = ColorFilter.tint(Color.White),
+                alignment = Alignment.TopStart,
+                contentDescription = "Quote",
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(180F)
+                    .background(Color.Black)
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column(modifier = Modifier.weight(1f)) {
+
+            }
+        }
+    }
+}
