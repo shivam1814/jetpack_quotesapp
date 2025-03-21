90bd41c8c0943a1e871c4e642dba889743d86c20
shivam <mandaliashivam72@gmail.com>
2025-03-21 23:13:23 +0530

    small changes in project by shivam

--- app/src/main/java/com/compose/quotesapp/screens/QuoteListItem.kt
+++ app/src/main/java/com/compose/quotesapp/screens/QuoteListItem.kt
@@ -1,27 +1,35 @@
 package com.compose.quotesapp.screens
 
-import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun QuoteListItem() {
     Card(
@@ -32,7 +40,7 @@ fun QuoteListItem() {
             modifier = Modifier.padding(16.dp)
         ) {
             Image(
-                imageVector = Icons.Filled.Close,
+                imageVector = Icons.Filled.FormatQuote,
                 colorFilter = ColorFilter.tint(Color.White),
                 alignment = Alignment.TopStart,
                 contentDescription = "Quote",
@@ -43,7 +51,74 @@ fun QuoteListItem() {
             )
             Spacer(modifier = Modifier.padding(4.dp))
             Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Time is the most valuable thing a man can spend.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .background(Color(0xFFEEEEEE))
+                        .fillMaxWidth(.4f)
+                        .height(1.dp)
+                )
+                Text(
+                    text = "Shivam mandalia",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
+        }
+    }
+}
 
+@Preview
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(4.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White // Change background color here
+            ),
+            modifier = Modifier
+                .padding(32.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start, // Use this instead of align()
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180f)
+                )
+                Text(
+                    text = "Time is the most valuable thing a man can spend",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    text = "Shivam Mandalia",
+                    style = MaterialTheme.typography.titleSmall
+                )
             }
         }
     }
